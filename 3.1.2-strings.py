98da7c6e604a1ce27590b1dd4ff1b1f7f2db31dc
Hai Huynh <hai.huynh@paradox.ai>
2020-07-01 17:06:39 +0700

    Strings
    * understanding basic strings

---
+++ 3.1.2-strings.py
@@ -0,0 +1,11 @@
+print('This is a single line string')
+print('This is a 2-line\nstring')
+print('''\
+this is a paragraph
+        line 2
+            line 3
+                ...
+                    line n 
+''')
+print('this is a formatted string with param1: {} and param2: {}'.format('1', 2))
+print('this is a capitalized string'.capitalize())
