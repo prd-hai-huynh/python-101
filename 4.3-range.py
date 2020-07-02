dc5d7e31312254cd6143a223da055e8e1fb2365e
Hai Huynh <hai.huynh@paradox.ai>
2020-07-02 09:56:30 +0700

    range statement
    * understanding range statement

---
+++ 4.3-range.py
@@ -0,0 +1,10 @@
+for i in range(5):
+    print(i)
+for i in range(0, 10, 2):
+    print(i)
+words = ['the', 'fox', 'jumped', 'over', 'the', 'lazy', 'dog']
+for i in range(len(words)):
+    print(i, words[i])
+print(list(range(5)))
+print(list(range(5, 10)))
+print(sum(range(4)))
